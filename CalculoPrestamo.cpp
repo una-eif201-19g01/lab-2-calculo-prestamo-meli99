0bbf7d681aefd9deb36b9874d76a34f542450a04
Meli <53483691+meli99@users.noreply.github.com>
2019-08-09 09:33:25 -0600

    clase

--- CalculoPrestamo.cpp
+++ CalculoPrestamo.cpp
@@ -4,3 +4,31 @@
 
 #include <iostream>
 #include "CalculoPrestamo.h"
+
+
+    float obtenerPorcentaje(string porcentajeTXT){
+    substrg(,)
+    std::stof()
+    return (porcentajeTXT/100);
+    }
+        //   Se utiliza solo para convertir años a meses, ejemplo:
+    int calcularTiempoEnMeses(string tiempoTXT){
+        if (tiempoTXT==M)
+            return (tiempoTXT);
+        else
+            if
+        int find= find ("A");
+        
+        return (tiempoTXT/12)
+    }
+       //   Se utiliza para calcular el interes mensual a partir de una tasa anual
+    float calcularInteresMensual(float balance, float tasaAnual){
+        return((tasaAnual/12)*monto);
+        
+ 
+    }
+
+   
+      //      Entra 30.000, 0.084 y sale 210
+  //  El método reporteCalculoPrestamo(std::string tiempoTXT, std::string porcentajeTXT) permite retornar en pantalla el resultado del cálculo de valores.
+  //      Entra 1A, 8.40% y sale el reporte en pantalla
--- CalculoPrestamo.h
+++ CalculoPrestamo.h
@@ -10,7 +10,14 @@
 #include <string>
 class CalculoPrestamo{
     float monto;
- 
+    CalculoPrestamo();
+    CalculoPrestamo(int64_t monto);
+    void setMonto(float monto);
+    float getMonto();
+    float obtenerPorcentaje(std::string porcentajeTXT);
+    int calcularTiempoEnMeses(std::string tiempoTXT);
+    float calcularInteresMensual(float balance, float tasaAnual);
+    reporteCalculoPrestamo(std::string tiempoTXT, std::string porcentajeTXT);
 };
 
 #endif //CALCULOPRESTAMO_CALCULOPRESTAMO_H
